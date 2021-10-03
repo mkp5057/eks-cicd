cdcf01f830870d388bb0c944029ca5b1faff9917
mkp5057 <manishkumar.pandey@outlook.com>
2021-10-03 19:02:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('EKS deployment v1!')
+  res.send('Say Hello to EKS v1!')
 })
 
 app.get('/status', (req, res) => {
